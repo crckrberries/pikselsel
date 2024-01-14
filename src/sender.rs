5884fdce2ac32f0f277dd1b4b8435c927028641a
crackerberries <crackedberries@gmail.com>
2024-01-14 19:04:37 +0300

    added logging :3

--- src/sender.rs
+++ src/sender.rs
@@ -2,9 +2,9 @@ use std::io::{BufWriter, Write};
 use std::net::TcpStream;
 
 pub fn send(commands: Vec<String>, host: &str) {
-    let stream = TcpStream::connect(host).unwrap();
-    let mut writer = BufWriter::new(&stream);
-    
+    let stream: TcpStream = TcpStream::connect(host).unwrap();
+    let mut writer: BufWriter<&TcpStream> = BufWriter::new(&stream);
+
     for cmd in commands {
         writer.write(cmd.as_bytes()).unwrap();
     }
